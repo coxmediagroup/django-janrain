d3813aa2c2c95b853576d3d0fc285f4cd35d0263
Nate Smith <nate.smith@coxinc.com>
2012-06-04 12:32:28 -0400

    more tests

--- janrain/tests/test_api.py
+++ janrain/tests/test_api.py
@@ -22,6 +22,12 @@ class TestAPI(TestCase):
             self.assertEqual(len(response.keys()), 1, 'got back our json')
             self.assertEqual(response['hello'], 'there', 'got back our json')
 
+    def test__make_request_get_args(self):
+        self.reqs.get = mock.Mock(return_value=MockRequestsJsonResponse(dict(hello='there')))
+        with mock.patch('janrain.api.requests', self.reqs):
+            self.client._make_request('path', data=dict(ohno='youdidnt'))
+            self.reqs.get.assert_called_with('path', params=dict(ohno='youdidnt', api_key='test_key'))
+
     def test__make_request_post(self):
         self.reqs.post = mock.Mock(return_value=MockRequestsJsonResponse(dict(you='guys')))
         with mock.patch('janrain.api.requests', self.reqs):
@@ -29,6 +35,12 @@ class TestAPI(TestCase):
 
             self.assertEqual(len(response.keys()), 1, 'got back our json')
             self.assertEqual(response['you'], 'guys', 'got back our json')
+
+    def test__make_request_post_args(self):
+        self.reqs.post = mock.Mock(return_value=MockRequestsJsonResponse(dict(you='guys')))
+        with mock.patch('janrain.api.requests', self.reqs):
+            self.client._make_request('path', method='post', data=dict(ohno='youdidnt'))
+            self.reqs.post.assert_called_with('path', data=dict(ohno='youdidnt', api_key='test_key'))
 
     def test__make_request_bad_method(self):
         self.assertRaises(ValueError, self.client._make_request, 'path', method='foobarbaz')
