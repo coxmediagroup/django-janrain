31f02c4597c73681da4dd85ca0eefb43bd383eb9
Nate Smith <nate.smith@coxinc.com>
2012-07-12 16:27:03 -0400

    typo

--- janrain/urls.py
+++ janrain/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^logout/$', JanrainLogoutView.as_view(), name='logout'),
     url(r'^loginpage/$', JanrainLoginPageView.as_view(), name='loginpage'),
     url(r'^xdcomm.html$', JanrainXDCommView.as_view(), name='xdcomm'),
-    url(r'^oauth_redirect$', JanrainOauthRedirectView.as_view(), name='xdcomm'),
+    url(r'^oauth_redirect$', JanrainOauthRedirectView.as_view(), name='oauth_redirect'),
 )
