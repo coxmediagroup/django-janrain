5d332dd9ff5edd89b822ec93368f45bda28f2d22
Nate Smith <nate.smith@coxinc.com>
2012-07-12 14:20:55 -0400

    update tests for JanrainUser

--- janrain/tests/test_backend.py
+++ janrain/tests/test_backend.py
@@ -2,12 +2,11 @@ from django.contrib.auth.models import User
 import mock
 from unittest2 import TestCase
 
-from janrain.backends import JanrainBackend
+from janrain.backends import JanrainBackend, JanrainUser
 
-class TestBackend(TestCase):
+class TestJanrainUserEngage(TestCase):
     def setUp(self):
-        self.backend = JanrainBackend()
-        self.auth_info = dict(
+        self.user_data = dict(
             profile=dict(
                 identifier='123456789',
                 name=dict(
@@ -20,57 +19,104 @@ class TestBackend(TestCase):
                 email='nate.smith@coxinc.com'
             ),
         )
+        self.user = JanrainUser(self.user_data)
+
+    def test_get_names_first_and_last(self):
+        fname, lname = self.user.names
+        self.assertEqual(fname, 'nate')
+        self.assertEqual(lname, 'smith')
+
+    def test_get_names_just_display_no_space(self):
+        del self.user_data['profile']['name']['givenName']
+        del self.user_data['profile']['name']['familyName']
+        user = JanrainUser(self.user_data)
+        fname, lname = user.names
+        self.assertEqual(fname, 'nathanielksmith')
+        self.assertEqual(lname, '')
+
+    def test_get_names_no_names(self):
+        del self.user_data['profile']['name']['givenName']
+        del self.user_data['profile']['name']['familyName']
+        del self.user_data['profile']['displayName']
+        user = JanrainUser(self.user_data)
+        fname, lname = user.names
+        self.assertEqual(fname, '')
+        self.assertEqual(lname, '')
+
+    def get_email(self):
+        del self.user_data['profile']['verifiedEmail']
+        user = JanrainUser(self.user_data)
+        self.assertEqual(user.email, 'nate.smith@coxinc.com')
+
+    def get_verified_email(self):
+        self.assertEqual(self.user.email, 'nathanielksmith@gmail.com')
+
+    def get_no_email(self):
+        del self.user_data['profile']['verifiedEmail']
+        del self.user_data['profile']['email']
+        user = JanrainUser(self.user_data)
+        self.assertEqual(user.email, '')
+
+# repeat above but for capture (entity) style data
+class TestJanrainUserCapture(TestCase):
+    def setUp(self):
+        self.user_data = dict(
+            uuid='123456789',
+            givenName='nate',
+            familyName='smith',
+            displayName='nathanielksmith',
+            email='nate.smith@coxinc.com',
+        )
+        self.user = JanrainUser(self.user_data)
+
+    def test_get_names_first_and_last(self):
+        fname, lname = self.user.names
+        self.assertEqual(fname, 'nate')
+        self.assertEqual(lname, 'smith')
+
+    def test_get_names_just_display_no_space(self):
+        del self.user_data['givenName']
+        del self.user_data['familyName']
+        user = JanrainUser(self.user_data)
+        fname, lname = user.names
+        self.assertEqual(fname, 'nathanielksmith')
+        self.assertEqual(lname, '')
+
+    def test_get_names_no_names(self):
+        del self.user_data['givenName']
+        del self.user_data['familyName']
+        del self.user_data['displayName']
+        user = JanrainUser(self.user_data)
+        fname, lname = user.names
+        self.assertEqual(fname, '')
+        self.assertEqual(lname, '')
+
+    def get_email(self):
+        self.assertEqual(self.user.email, 'nate.smith@coxinc.com')
+
+    def get_no_email(self):
+        del self.user_data['email']
+        user = JanrainUser(self.user_data)
+        self.assertEqual(user.email, '')
+
+class TestBackend(TestCase):
+    def setUp(self):
+        self.backend = JanrainBackend()
         muser = mock.Mock()
         muser.DoesNotExist = User.DoesNotExist
         muser.objects = mock.Mock()
         muser.objects.get = mock.Mock()
         self.muser = muser
 
+        self.user = mock.Mock()
+
     def test_find_user_found(self):
         with mock.patch('janrain.backends.User', self.muser):
             self.muser.objects.get.return_value = True
-            self.assertTrue(self.backend.find_user(self.auth_info))
+            self.assertTrue(self.backend.find_user(self.user))
 
     def test_find_user_not_found(self):
         with mock.patch('janrain.backends.User', self.muser):
             self.muser.objects.get.side_effect = self.muser.DoesNotExist
-            self.assertEqual(self.backend.find_user(self.auth_info), None)
+            self.assertEqual(self.backend.find_user(self.user), None)
 
-    def test_get_names_first_and_last(self):
-        fname, lname = self.backend.get_names(self.auth_info)
-        self.assertEqual(fname, 'nate')
-        self.assertEqual(lname, 'smith')
-
-    def test_get_names_just_display_no_space(self):
-        del self.auth_info['profile']['name']['givenName']
-        del self.auth_info['profile']['name']['familyName']
-
-        fname, lname = self.backend.get_names(self.auth_info)
-        self.assertEqual(fname, 'nathanielksmith')
-        self.assertEqual(lname, '')
-
-    def test_get_names_no_names(self):
-        del self.auth_info['profile']['name']['givenName']
-        del self.auth_info['profile']['name']['familyName']
-        del self.auth_info['profile']['displayName']
-
-        fname, lname = self.backend.get_names(self.auth_info)
-        self.assertEqual(fname, '')
-        self.assertEqual(lname, '')
-
-    def get_email(self):
-        del self.auth_info['profile']['verifiedEmail']
-
-        email = self.backend.get_email(self.auth_info)
-        self.assertEqual(email, 'nate.smith@coxinc.com')
-
-    def get_verified_email(self):
-        email = self.backend.get_email(self.auth_info)
-        self.assertEqual(email, 'nathanielksmith@gmail.com')
-
-    def get_no_email(self):
-        del self.auth_info['profile']['verifiedEmail']
-        del self.auth_info['profile']['email']
-
-        email = self.backend.get_email(self.auth_info)
-        self.assertEqual(email, '')
